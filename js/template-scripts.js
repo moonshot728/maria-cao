6b6f082e02d1bfb3e0658ff2ec68e7521aacdda9
HappyCodingLover <moonshot191@gmail.com>
2023-08-28 12:01:18 -0700

    update

--- js/template-scripts.js
+++ js/template-scripts.js
@@ -68,35 +68,4 @@ jQuery(document).ready(function ($) {
     animateOut: "fadeOut",
     autoplayTimeout: 4000,
   });
-
-  // Sticky Nav Bar
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 20) {
-      $(".sticky").addClass("fixed");
-    } else {
-      $(".sticky").removeClass("fixed");
-    }
-
-    var transY = $(this).scrollTop();
-    $(".hero-v1-image-center").css(
-      "transform",
-      `translate3d(0px, -${
-        (transY * 2) / 100
-      }%, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)`
-    );
-
-    $("#w-node-_4e9aecdf-ea6d-1837-fab4-96e135d16c83-0a989a75").css(
-      "transform",
-      `translate3d(0px, ${
-        transY / 100
-      }%, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)`
-    );
-    $("#w-node-_11d23e0e-018c-8183-617a-f9839add8397-0a989a75").css(
-      "transform",
-      `translate3d(0px, ${
-        transY / 100
-      }%, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)`
-    );
-    console.log($(this).scrollTop());
-  });
 });
